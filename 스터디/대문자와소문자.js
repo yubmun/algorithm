7427c45b76e78576d83ffd7d889b016aa23d0d79
yubmun <dlwnsduq23@gmail.com>
2023-01-01 23:11:16 +0900

    소문자 대문자 변환

---
+++ 스터디/대문자와소문자.js
@@ -0,0 +1,15 @@
+/**
+ * 문자열 my_string이 매개변수로 주어질 때, 대문자는 소문자로 소문자는 대문자로 변환한 문자열을 return하도록 solution 함수를 완성해주세요.
+ */
+
+function solution (string){
+  var answer = "";
+  for (let i=0; i < string.length; i++){
+    if(string[i] === string[i].toUpperCase()){
+      answer += string[i].toLowerCase();
+    } else {
+    answer += string[i].toUpperCase();
+    }  
+  }
+  return answer
+}
